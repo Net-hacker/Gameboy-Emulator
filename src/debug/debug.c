a31475994e56c5dc6e4281de21879467490a6c02
Nethack <85522443+Net-hacker@users.noreply.github.com>
2025-11-08 11:30:06 +0100

    Es funktioniert wieder
    Opcodes bis 0x7F und Kommentare.
    Ich habe in cpu.c vergessen opcode = rom[cpu.PC] zu setzen, deswegen hat es Probleme gegeben

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -3,6 +3,15 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+ * Überprüfung ob zwei Strings gleich sind.
+ *
+ * Ich wollte einfach eine Möglichkeit zum Checken, weil es mir zu repetitiv wurde.
+ *
+ * @param a Die erste String.
+ * @param b Die zweite String.
+ * @return Ob die beiden Strings gleich sind oder nicht (true/false).
+ */
 bool checkString(const char* a, const char* b)
 {
   if (strcmp(a, b) == 0)
@@ -11,6 +20,14 @@ bool checkString(const char* a, const char* b)
     return false;
 }
 
+/**
+ * Formatierte Ausgabe einer Meldung.
+ *
+ * Eine Debugmöglichkeit, falls Meldungen ausgegeben werden sollen.
+ *
+ * @param info Der Inhalt der Meldung-
+ * @param format Hier und danach ist es wie bei printf.
+ */
 void printDebug(const char* info, const char* format, ...)
 {
   printf("\033[33;40;1m[Debug]:\033[0m %s", info);
@@ -23,6 +40,14 @@ void printDebug(const char* info, const char* format, ...)
   printf("\n");
 }
 
+/**
+ * Formatierte Ausgabe einer Fehlermeldung.
+ *
+ * Eine Debugmöglichkeit, falls Fehler auftreten sollten.
+ *
+ * @param error Die Fehlermeldungen, welche ausgegeben werden soll.
+ * @param format Hier und danach ist es wie bei printf.
+ */
 void printError(const char* error, const char* format, ...)
 {
   printf("\033[31;40;1mError\033[0m %s\n", error);
--- src/rom/rom.c
+++ src/rom/rom.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Einlesung der .gb Datei in eine Variable.
+ *
+ * Hier wird die .gb Datei eingelesen und in eine Variable gespeichert um damit weiterzuarbeiten.
+ *
+ * @param *file Die Datei als FILE.
+ * @return Eine unsigned char* Variable mit dem Inhalt der .gb Datei.
+ */
 unsigned char* ReadFile(FILE *file)
 {
   fseek(file, 0, SEEK_END);
